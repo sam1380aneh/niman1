b45b5050ac195e46b889a44072f31a50dc5693b9
sam1380ane <sam1380aneh@gmail.com>
2025-12-23 19:16:17 +0330

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import "./base.css";
 import "tailwindcss";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import "./App.css";
